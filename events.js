d9de2e13c80251b94849022bf9d575b8e006e4b1
ElvisNascimento <elvisnascimentodev@gmail.com>
2022-10-02 02:44:59 -0300

    finzalido

--- events.js
+++ events.js
@@ -24,23 +24,45 @@ function atualizarLista() {
         lista.forEach(function (cadaContato) {
             contatos.innerHTML += 
             `
-                <div class="row">
-                        <div class="col-10 bg-primary">
-                            <div class="row">
-                                <div class="col-6">
-                                ${cadaContato.id}${cadaContato.contato} ${cadaContato.telefone} ${cadaContato.cidade}
+            <div class="col-12 accordion accordion-flush mb-1" id="accordionFlushExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-heading${cadaContato.id}">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${cadaContato.id}" aria-expanded="false" aria-controls="flush-collapse${cadaContato.id}"">
+                            <span hidden>${cadaContato.id}</span>${cadaContato.contato}
+                        </button>
+                    </h2>
+                        <div id="flush-collapse${cadaContato.id}"" class="accordion-collapse collapse" aria-labelledby="flush-heading${cadaContato.id}" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                            
+                            <div class="row align-items-center ">
+                            
+                            <!-- coluna dos dados -->
+                            <div class="col-2">
+                                <img src="https://i.pinimg.com/originals/07/15/37/071537f20f9d05d96f9846e098d04c45.jpg" class="rounded-circle w-75" alt="...">
+                            </div>
+                            <div class="col-6">
+                                <div class="row text-capitalize w-100">
+                                    <span hidden>${cadaContato.id}</span>
+                                    <span >${cadaContato.contato}</span>
+                                    <span >${cadaContato.telefone}</span>
+                                    <span >${cadaContato.cidade}</span>
+                                </div>
+                            </div>
+                            <!-- coluna dos botoes -->
+                            <div class="col-4">
+                                <div class="row">
+                                    <div class="btn-group text-center" role="group">
+                                        <button onclick="editarContato(${cadaContato.id})" class="btn btn-outline-warning  w-50" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar">Editar</button>
+                                        <button onclick="excluirContato(${cadaContato.id})" class="btn btn-outline-danger  w-50">Excluir</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    <div class="col-2 bg-info">
-                        <div class="row">
-                            <div class="btn-group" role="group">
-                                <button onclick="editarContato(${cadaContato.id})" class="btn btn-warning" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEditar">Editar</button>
-                                <button onclick="excluirContato(${cadaContato.id})" class="btn btn-danger">Excluir</button>
+                            
+                            </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </div> 
             `;
         });
     })
@@ -51,7 +73,8 @@ async function inserirNovocontato(){
         contato: input_contato.value,
         telefone: parseInt(input_telefone.value),    
     };
-    if(dados.contato !== '' && dados.telefone !==''){
+    
+    if(dados.contato !== '' && dados.telefone !== ''){
         await fetch(url,{
             method: 'POST',
             body: JSON.stringify(dados),// convertendo dados que é um objeto em um novo array de string
